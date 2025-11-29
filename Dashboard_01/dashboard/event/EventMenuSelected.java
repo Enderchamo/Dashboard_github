fbe0aa9b8a910900e740324bd60556faa2e3b0ef
Vic <vic@Viccomputadora.localdomain>
2025-11-29 15:11:20 -0400

    Coloreado

---
+++ Dashboard_01/dashboard/event/EventMenuSelected.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dashboard.event;
+
+/**
+ *
+ * @author Vicma
+ */
+public interface EventMenuSelected {
+
+    public void menuSelected(int menuIndex, int subMenuIndex);
+}
+
---
+++ Dashboard_01/dashboard/main/Main.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package com.dashboard.main;
+
+import com.dashboard.complement.Header;
+import com.dashboard.complement.Menu;
+import com.dashboard.event.EventMenuSelected;
+import com.dashboard.form.MainForm;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author Vicma
+ */
+public class Main extends javax.swing.JFrame {
+    
+    private MigLayout layout;
+    private Menu menu;
+    private Header header;
+    private MainForm main;
+
+    public Main() {
+        initComponents();
+        init();
+    }
+    
+    private void  init(){
+    
+        layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
+        bg.setLayout(layout);
+        menu = new Menu();
+        header = new Header();
+        main = new MainForm();
+        menu.addEvent(new EventMenuSelected() {
+             @Override
+             public void menuSelected(int menuIndex, int subMenuIndex) {
+                 System.out.println("Menu Index : " + menuIndex + " SubMenu Index " + subMenuIndex);
+                 
+             }
+         });
+        menu.initMenuItem();
+        bg.add(menu, "w 230!, spany 2");    // Span Y 2cell
+        bg.add(header, "h 50!, wrap");
+        bg.add(main, "w 100%, h 100%");
+        
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JLayeredPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setForeground(new java.awt.Color(0, 0, 0));
+        bg.setOpaque(true);
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1256, Short.MAX_VALUE)
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 705, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(bg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+      
+        java.awt.EventQueue.invokeLater(() -> new Main().setVisible(true));
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLayeredPane bg;
+    // End of variables declaration//GEN-END:variables
+}
